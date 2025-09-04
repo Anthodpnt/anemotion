10d55bc48dac50ed1f0034981fd5dd371fb15ad6
Anthony Du Pont <a.dupont@evs.com>
2025-09-04 17:43:01 +0200

    chore: add locomotive-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { ReactLenis } from 'lenis/react'
+import { useEffect } from 'react'
+import LocomotiveScroll from 'locomotive-scroll'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SectionTitle from './components/SectionTitle'
 
 const App = () => {
+  useEffect(() => {
+    new LocomotiveScroll()
+  }, [])
+
   return (
     <>
-      <ReactLenis root />
-
       <Header />
 
-      <SectionTitle title="Slide 1" subtitle="Lorem ipsum dolor sit amet." />
-      <SectionTitle title="Slide 2" subtitle="Lorem ipsum dolor sit amet." />
-      <SectionTitle title="Slide 3" subtitle="Lorem ipsum dolor sit amet." />
-      <SectionTitle title="Slide 4" subtitle="Lorem ipsum dolor sit amet." />
-      <SectionTitle title="Slide 5" subtitle="Lorem ipsum dolor sit amet." />
+      <div>
+        <SectionTitle title="Slide 1" subtitle="Lorem ipsum dolor sit amet." />
+        <SectionTitle title="Slide 2" subtitle="Lorem ipsum dolor sit amet." />
+        <SectionTitle title="Slide 3" subtitle="Lorem ipsum dolor sit amet." />
+        <SectionTitle title="Slide 4" subtitle="Lorem ipsum dolor sit amet." />
+        <SectionTitle title="Slide 5" subtitle="Lorem ipsum dolor sit amet." />
+      </div>
 
       <Footer />
     </>
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -6,8 +6,27 @@ const SectionTitle = ({ title, subtitle }) => {
   return (
     <section className={s.section}>
       <Container>
-        <h2 className={s.title}>{title}</h2>
-        {subtitle && <p className={s.subtitle}>{subtitle}</p>}
+        <h2
+          className={s.title}
+          data-scroll
+          data-scroll-repeat
+          data-scroll-position="middle,middle"
+          data-scroll-css-progress
+        >
+          {title}
+        </h2>
+
+        {subtitle && (
+          <p
+            className={s.subtitle}
+            data-scroll
+            data-scroll-repeat
+            data-scroll-position="middle,middle"
+            data-scroll-css-progress
+          >
+            {subtitle}
+          </p>
+        )}
       </Container>
     </section>
   )
