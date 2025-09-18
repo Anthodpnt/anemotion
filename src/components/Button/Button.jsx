51744167bb127b2c3b465d721760c4815b308a69
Anthony Du Pont <a.dupont@evs.com>
2025-09-18 14:23:08 +0200

    feat: add micro interactions

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,28 +1,72 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 import cn from 'clsx'
 
 import s from './Button.module.scss'
 
 const Button = ({ children, className, ...props }) => {
   const Tag = props.type === 'button' ? 'button' : 'span'
-  const [angle, setAngle] = useState(0)
+  const button = useRef()
 
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      const center = window.innerWidth / 2
-      const progress = (e.clientX - center) / center
-      setAngle(progress * -60)
+  // Motion - Light and magnetic animation.
+  const [angle, setAngle] = useState(0)
+  const { contextSafe } = useGSAP()
+
+  const handleMouseMove = contextSafe((e) => {
+    const bounds = button.current.getBoundingClientRect()
+
+    if (bounds.top >= window.innerHeight || bounds.bottom <= 0) {
+      gsap.set(button.current, { clearProps: 'all' })
+      return
     }
 
+    // Move the light based on the cursor angle.
+    const center = window.innerWidth / 2
+    const progress = (e.clientX - center) / center
+
+    setAngle(progress * 45)
+
+    // Magnetic effect.
+    const centerX = bounds.left + bounds.width / 2
+    const centerY = bounds.top + bounds.height / 2
+    const distance = Math.hypot(e.clientX - centerX, e.clientY - centerY)
+
+    if (distance < 150) {
+      const strength = (1 - distance / 150) * 12
+      const angle = Math.atan2(e.clientY - centerY, e.clientX - centerX)
+
+      let x = Math.cos(angle) * strength
+      let y = Math.sin(angle) * strength
+
+      if (distance < bounds.width / 2) {
+        x = e.clientX - centerX
+        y = e.clientY - centerY
+      }
+
+      gsap.to(button.current, {
+        x,
+        y,
+        ease: 'power3.out',
+        duration: 0.6,
+      })
+    } else {
+      gsap.set(button.current, {
+        clearProps: 'x,y',
+      })
+    }
+  })
+
+  useEffect(() => {
     window.addEventListener('mousemove', handleMouseMove)
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
     }
-  }, [])
+  }, [handleMouseMove])
 
   return (
-    <Tag {...props} style={{ '--angle': `${angle}deg` }} className={cn(s.button, className)}>
+    <Tag {...props} ref={button} style={{ '--angle': `${angle}deg` }} className={cn(s.button, className)}>
       {children}
     </Tag>
   )
--- src/components/Caption/Caption.jsx
+++ src/components/Caption/Caption.jsx
@@ -1,27 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 import cn from 'clsx'
 
 import s from './Caption.module.scss'
 
 const Caption = ({ children, className }) => {
-  const [offset, setOffset] = useState(0)
+  const caption = useRef()
 
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      const center = window.innerWidth / 2
-      const progress = (e.clientX - center) / center
-      setOffset(progress * -1)
+  // Motion - Light animation.
+  const [offset, setOffset] = useState(0)
+  const { contextSafe } = useGSAP()
+
+  const handleMouseMove = contextSafe((e) => {
+    const bounds = caption.current.getBoundingClientRect()
+
+    if (bounds.top >= window.innerHeight || bounds.bottom <= 0) {
+      gsap.set(caption.current, { clearProps: 'all' })
+      return
     }
 
+    // Move the light based on the cursor position.
+    const center = window.innerWidth / 2
+    const progress = (e.clientX - center) / center
+
+    setOffset(progress * -1)
+  })
+
+  useEffect(() => {
     window.addEventListener('mousemove', handleMouseMove)
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
     }
-  }, [])
+  }, [handleMouseMove])
 
   return (
-    <p style={{ '--fade': `${Math.abs(offset)}px`, '--offset': `${offset}px` }} className={cn(s.caption, className)}>
+    <p
+      ref={caption}
+      style={{ '--fade': `${Math.abs(offset)}px`, '--offset': `${offset}px` }}
+      className={cn(s.caption, className)}
+    >
       {children}
     </p>
   )
