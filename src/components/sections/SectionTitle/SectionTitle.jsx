0085ae950a6594864e91778119523172a072bcf1
Anthony Du Pont <a.dupont@evs.com>
2025-10-03 17:29:41 +0200

    feat: fine tune UI

--- src/components/sections/SectionTitle/SectionTitle.jsx
+++ src/components/sections/SectionTitle/SectionTitle.jsx
@@ -36,18 +36,16 @@ const Media = ({ src }) => {
   }, [id])
 
   return (
-    <div className={s.video} data-scroll data-scroll-call={id} data-scroll-repeat data-scroll-position="middle,middle">
-      <video ref={video} loop muted playsInline>
-        {src.map((_src, index) => (
-          <source key={`video-source__${index}`} src={_src} type={`video/${_src.split('.').pop()}`} />
-        ))}
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <video ref={video} loop muted playsInline data-scroll data-scroll-call={id}>
+      {src.map((_src, index) => (
+        <source key={`video-source__${index}`} src={_src} type={`video/${_src.split('.').pop()}`} />
+      ))}
+      Your browser does not support the video tag.
+    </video>
   )
 }
 
-const SectionTitle = ({ videos, layout = 'square', title, subtitle }) => {
+const SectionTitle = ({ images, videos, layout = 'square', title, subtitle }) => {
   const root = useRef()
   const header = useRef()
 
@@ -63,9 +61,33 @@ const SectionTitle = ({ videos, layout = 'square', title, subtitle }) => {
         </header>
 
         {videos && videos.length > 0 && (
-          <div className={s.videos} data-scroll data-scroll-speed="0.1">
+          <div className={s.medias} data-scroll data-scroll-speed="0.1">
             {videos.map((video, index) => (
-              <Media key={`video-${index}`} src={video} />
+              <div
+                key={`video-${index}`}
+                className={s.media}
+                data-scroll
+                data-scroll-repeat
+                data-scroll-position="middle,middle"
+              >
+                <Media src={video} />
+              </div>
+            ))}
+          </div>
+        )}
+
+        {images && images.length > 0 && (
+          <div className={s.medias} data-scroll data-scroll-speed="0.1">
+            {images.map((image, i) => (
+              <div
+                key={`image-${i}`}
+                className={s.media}
+                data-scroll
+                data-scroll-repeat
+                data-scroll-position="middle,middle"
+              >
+                <img {...image} />
+              </div>
             ))}
           </div>
         )}
--- src/data/simple.js
+++ src/data/simple.js
@@ -2,9 +2,9 @@ export default {
   title: 'Simple Animations',
   subtitle: 'Improve the User Experience',
   videos: [
-    ['/videos/simple-animation-1.mp4'],
-    ['/videos/simple-animation-2.mp4'],
-    ['/videos/simple-animation-3.mp4'],
-    ['/videos/simple-animation-4.mp4'],
+    [`${import.meta.env.VITE_CDN_URL}/videos/simple-animation-1.mp4`],
+    [`${import.meta.env.VITE_CDN_URL}/videos/simple-animation-2.mp4`],
+    [`${import.meta.env.VITE_CDN_URL}/videos/simple-animation-3.mp4`],
+    [`${import.meta.env.VITE_CDN_URL}/videos/simple-animation-4.mp4`],
   ],
 }
